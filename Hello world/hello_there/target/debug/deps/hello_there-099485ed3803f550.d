2d6e49a98da145c6ca0349d29f77504155b4857d
AMIT PATEL <2202031000081@silveroakuni.ac.in>
2023-12-14 13:19:34 +0530

    initializing repo

---
+++ Hello world/hello_there/target/debug/deps/hello_there-099485ed3803f550.d
@@ -0,0 +1,5 @@
+c:\Users\Amit Patel\Desktop\Repo-with-Graphql\Hello world\hello_there\target\debug\deps\libhello_there-099485ed3803f550.rmeta: src\main.rs
+
+c:\Users\Amit Patel\Desktop\Repo-with-Graphql\Hello world\hello_there\target\debug\deps\hello_there-099485ed3803f550.d: src\main.rs
+
+src\main.rs:
---
+++ Hello world/hello_there/target/debug/deps/hello_there-98d5cab4452f40ee.d
@@ -0,0 +1,5 @@
+c:\Users\Amit Patel\Desktop\Repo-with-Graphql\Hello world\hello_there\target\debug\deps\libhello_there-98d5cab4452f40ee.rmeta: src\main.rs
+
+c:\Users\Amit Patel\Desktop\Repo-with-Graphql\Hello world\hello_there\target\debug\deps\hello_there-98d5cab4452f40ee.d: src\main.rs
+
+src\main.rs:
